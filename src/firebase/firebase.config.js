46327da5f65bb72169377295cbf63d6cfc8975e7
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-02 20:35:10 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDHrAlnIRhRfggFRNXedUtlcs_B77se2u0",
+  authDomain: "mhnmea.firebaseapp.com",
+  projectId: "mhnmea",
+  storageBucket: "mhnmea.appspot.com",
+  messagingSenderId: "186401842551",
+  appId: "1:186401842551:web:f6bb38bf48875e00d0ad72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
